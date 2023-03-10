beda5f5de2fd5b5c1aeb36ef22d4dabf390bde02
Cole Gibson <coleggibson@gmail.com>
2023-03-10 15:40:24 -0600

    Added styling to results and search container

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,41 +3,23 @@ import uniqid from "uniqid";
 import '../index.css'
 import '../styles/Content.css'
 
-
+//add count to ingredients
+//add random recipe
 const Content = () => {
     
-    let orderNum = 3
+    let orderNum = 2
     const [ingredients, setIngredients] = useState ([
-        {name: 'apple', id:uniqid()},
-        {name: 'orange', id:uniqid()},
-        {name: 'chicken', id:uniqid()}
+        {name: 'Apple', id:uniqid()},
+        {name: 'Orange', id:uniqid()},
+        {name: 'Chicken', id:uniqid()}
+    ]);
+
+    const [recipes, setRecipes] = useState([
+        {title: 'Cornmeal mix for a big family', link:'www.cornmeal.com', id:123},
+        {title: 'Carrot soufle for the big fam on board',link:'www.carrot.com', id:125},
     ]);
 
     
-
-
-
-    function combineIngredients (ingredients) {
-        let urlIngredientArray = '';
-        for (let i =0; i < ingredients.length; i++) {
-            urlIngredientArray += ingredients[i].name + ',+'
-        }
-
-        let newUrl = 'https:api.spoonacular.com/recipes/findByIngredients?apiKey=5a9eaad9b6f54ec5adca1041255d83f2&ingredients=' + urlIngredientArray.toLowerCase() + '&number=10' ; 
-        
-        // fetch(newUrl) 
-    
-        // .then(function(response) {
-        //     return response.json();
-        // })
-        
-        // .then(function(response) {
-        //     console.log(response);
-        // });
-        
-        console.log(urlIngredientArray)
-        console.log(newUrl)
-    }
 
     const capitalize = (string) => {
         let lowercaseResult = string.toLowerCase();
@@ -46,18 +28,58 @@ const Content = () => {
     }
 
     const addIngredient = (ingredient) => {
+        let value = document.getElementById('name').value
         let ingredientObject = {}
-        
-        
         ingredientObject.name = capitalize(ingredient)
+        
+        if ( value.trim() !== ''){
         ingredientObject.id = uniqid()
         ingredientObject.orderNum = orderNum + 1
         ingredients.push(ingredientObject)
         setIngredients([...ingredients])
-        document.getElementById('name').value = ''
-        console.log(ingredients)
+        combineIngredients(ingredients)
+        value = ''
+        
+        }
     }
 
+    const combineIngredients = (ingredients) => {
+        let urlIngredientArray = '';
+        for (let i =0; i < ingredients.length; i++) {
+            urlIngredientArray += ingredients[i].name + ',+'
+        }
+
+        let newUrl = 'https:api.spoonacular.com/recipes/findByIngredients?apiKey=5a9eaad9b6f54ec5adca1041255d83f2&ingredients=' + urlIngredientArray.toLowerCase() + '&number=3' ; 
+        
+        fetch(newUrl) 
+    
+        .then(function(result) {
+            return result.json();
+        })
+        
+        .then(function(result) {
+            setRecipes([...result])
+        });
+
+        
+        console.log(recipes)
+    }
+    
+    const getRecipeUrl = (recipe) => {
+        let recipeId = recipe.id
+        let recipeUrl = 'https:api.spoonacular.com/recipes/' + recipeId + '/information?apiKey=5a9eaad9b6f54ec5adca1041255d83f2'; 
+        
+        fetch(recipeUrl) 
+    
+        .then(function(result) {
+            return result.json();
+        })
+        
+        .then(function(result) {
+            recipe.link = result.sourceUrl
+            setRecipes([...recipes])
+        });
+    }
 
     const deleteIngredient = (ingredient) => {
         
@@ -68,8 +90,6 @@ const Content = () => {
         setIngredients([...ingredients])
     }
 
-
-    combineIngredients(ingredients)
     return (
         <div id="content-container">
             <div id='search-content'>
@@ -86,8 +106,24 @@ const Content = () => {
                         )
                     })}
                 </div>
+                
             </div>
-            <div id='recipe-results'>!recipe results</div>
+            
+            <div id='recipe-results'>
+            <div id='results-title'>Results</div>
+                    {recipes.map((recipe) => {
+                        return (<div key={recipe.id} 
+                                className='recipe-container'>
+                                <div className='recipe-image'>img</div>
+                                <div className='recipe-content-container'>
+                                    <div className='recipe-name'>{recipe.title}</div>
+                                    <div className ='recipe-url'>Link to recipe: {recipe.link}</div>
+                                </div>
+                        </div>
+                        )
+                    })}
+                
+            </div>
         </div>
     )
 }
