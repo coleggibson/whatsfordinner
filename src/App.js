ead7aa9fd2e9bae8e2bbf90dfa1822914851087b
Cole Gibson <coleggibson@gmail.com>
2023-03-09 21:25:31 -0600

    Added button to add ingredient to array on click

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import React from "react";
 import './styles/App.css';
 import Header from "./components/Header";
 import Content from "./components/Content";
+import './index.css'
 
 function App() {
 
-  const [data, setData] = React.useState(null)
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
-  }, []);
 
   return (
     <div className="App">
--- src/components/Content.js
+++ src/components/Content.js
@@ -1,20 +1,60 @@
 import { useState } from "react"
 import uniqid from "uniqid";
+import '../index.css'
+import '../styles/Content.css'
 
 
 const Content = () => {
     
     
-
     const [ingredients, setIngredients] = useState ([
         {name: 'apple', id:uniqid()},
         {name: 'orange', id:uniqid()},
         {name: 'chicken', id:uniqid()}
     ]);
 
+    
+
+
+
+    function combineIngredients (ingredients) {
+        let urlIngredientArray = '';
+        for (let i =0; i < ingredients.length; i++) {
+            urlIngredientArray += ingredients[i].name + ',+'
+        }
+
+        let newUrl = 'https:api.spoonacular.com/recipes/findByIngredients?apiKey=5a9eaad9b6f54ec5adca1041255d83f2&ingredients=' + urlIngredientArray + '&number=10' ; 
+        
+        // fetch(newUrl) 
+    
+        // .then(function(response) {
+        //     return response.json();
+        // })
+        
+        // .then(function(response) {
+        //     console.log(response);
+        // });
+        
+        console.log(urlIngredientArray)
+        console.log(newUrl)
+    }
+
+    const addIngredient = (ingredient) => {
+        let ingredientObject = {}
+        ingredientObject.name = ingredient
+        ingredientObject.id = uniqid()
+        ingredients.push(ingredientObject)
+        setIngredients([...ingredients])
+        document.getElementById('name').value = ''
+        console.log(ingredients)
+    }
+
+    combineIngredients(ingredients)
     return (
         <div id="content-container">
-            <input type='text' placeholder='Search for ingredient' id='ingredient-searchbar'></input>
+            <input id='name' type='text' defaultValue='' placeholder='Add ingredient'/>
+            <br/>
+            <input type='button' value='Submit' onClick={() => addIngredient(document.getElementById('name').value)}/>
             <div id='ingredient-list'>
                 {ingredients.map((ingredient) => {
                     return (<div key={ingredient.id} 
@@ -24,20 +64,9 @@ const Content = () => {
                     )
                 })}
             </div>
+            <div id='recipe-results'>!recipe results</div>
         </div>
     )
 }
 
 export default Content
-
-{/* <div id="cardContainer">
-        {cards.map((card) => {
-            return (<div key={card.id} 
-                    className='cardDiv' 
-                    onClick={() => {shuffleCards(cards); keepScore(card.id);}}>
-                        <img className ='cardImg'src={card.imgsrc}></img>
-                        <div className='card-title'>{card.title}</div>
-                   </div>
-            )
-        })}
-    </div> */}
